ec6cf18cc2bf59ae9310c1862dbf47252423e13a
izumi-hiro <transformers2728frost@gmail.com>
2022-04-26 05:31:11 +0000

    [Add] models/users.rb_has_many :books

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :books
+  
   has_one_attached :profile_image
+  has_many :books
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
 
