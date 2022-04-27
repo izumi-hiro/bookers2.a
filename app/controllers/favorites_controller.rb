dfb0b70e5f6003cb826b2a6c0a585c0400481e5a
izumi-hiro <transformers2728frost@gmail.com>
2022-04-27 09:30:23 +0000

    [Add] favorites#destroy

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,5 +8,10 @@ class FavoritesController < ApplicationController
   end
   
   def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_to book_path(book)
   end
+  
 end
