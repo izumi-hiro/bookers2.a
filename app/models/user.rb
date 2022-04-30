d4b01040540872aec0077c6b1af5dc7f75622cdc
izumi-hiro <transformers2728frost@gmail.com>
2022-04-30 09:12:34 +0000

    [Add] models/user.rb method

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,13 +17,23 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true, presence: true
   validates :introduction, length: { maximum: 50 }
 
-
-
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg' )
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
   end
 end
