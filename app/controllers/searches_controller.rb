99f8ad74dc26d17367a1cf9e95bdff2c0f221a15
izumi-hiro <transformers2728frost@gmail.com>
2022-05-02 05:22:03 +0000

    [Finish] search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,11 +3,11 @@ class SearchesController < ApplicationController
   
   def search
     @range = params[:range]
+    @word = params[:word]
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
       @books = Book.looks(params[:search], params[:word])
     end
-    redirect_to search_path
   end
 end
